302702a5fbfd90834616698b4e9d1050ea333511
Arbaz-Khan19e <Arbaz.Ahmed@teknopoint.local>
2025-12-29 19:00:54 +0530

    blogs component added

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -1,4 +1,30 @@
+import { div, p } from "../../scripts/dom-helpers.js";
 
 export default function decorate(block) {
-  console.log("active",block);
+  console.log("active", block);
+  // const properties = readBlockConfig(block);
+  let textContent = block.querySelector("p") || "not found";
+  const blogs =
+    div({
+        class: "blogs block",
+        "data-block-name": "blogs",
+        "data-block-status": "loading"
+      },
+      div({
+          class: "main-wrapper"
+        },
+        div({
+            class: "main-wrapper1"
+          },
+          p({class:"title"},textContent),
+        ),
+      )
+    );
+
+    document.addEventListener("load", function() {
+      console.log("hi");
+    })
+    
+     block.innerHTML = '';
+  block.appendChild(blogs);
 }
