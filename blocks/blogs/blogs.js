5a8ca8fcb53a1e49f007a0b340f3b162275cc41d
Arbaz-Khan19e <Arbaz.Ahmed@teknopoint.local>
2025-12-30 13:09:22 +0530

    added image

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -5,21 +5,59 @@ export default function decorate(block) {
   // const properties = readBlockConfig(block);
   let textContent = block.querySelector("p") || "not found";
   const blogs =
-    div({
-        class: "blogs block",
-        "data-block-name": "blogs",
-        "data-block-status": "loading"
-      },
-      div({
-          class: "main-wrapper"
-        },
-        div({
-            class: "main-wrapper1"
-          },
-          p({class:"title"},textContent),
-        ),
+  div({
+      class: "blogs block",
+      "data-block-name": "blogs",
+      "data-block-status": "loading"
+    },
+
+    // 1) Title -> "Arbaz"
+    div({}, 
+      div({},
+        p({
+          "data-aue-prop": "title",
+          "data-aue-label": "Title",
+          "data-aue-type": "text"
+        }, "Arbaz")
       )
-    );
+    ),
+
+    // 2) Empty div
+    div({}, div({})),
+
+    // 3) Empty div
+    div({}, div({})),
+
+    // 4) Empty div
+    div({}, div({})),
+
+    // 5) Button container
+    div({},
+      div({},
+        p({ class: "button-container" },
+          a({
+              href: "/content/dept-crossIndustry/footer.html",
+              title: "/content/dept-crossIndustry/footer",
+              class: "button"
+            },
+            "/content/dept-crossIndustry/footer"
+          )
+        )
+      )
+    ),
+
+    // 6) CTA Style -> "button-dark"
+    div({},
+      div({},
+        p({
+          "data-aue-prop": "ctastyle",
+          "data-aue-label": "CTA Style",
+          "data-aue-type": "text"
+        }, "button-dark")
+      )
+    )
+  );
+
 
     document.addEventListener("load", function() {
       console.log("hi");
