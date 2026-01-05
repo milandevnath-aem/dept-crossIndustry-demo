d1be4fdfe623ccffa23a702b3d9bad4faac44672
Arbaz-Khan19e <Arbaz.Ahmed@teknopoint.local>
2026-01-05 17:18:54 +0530

    added banner

--- blocks/promotionalbanner/promotionalbanner.js
+++ blocks/promotionalbanner/promotionalbanner.js
@@ -4,18 +4,29 @@ export default function decorate(block) {
     console.log(block);
     // bannerType1();
     let getType = block.classList;
-     block.textContent = '';
             //  block.append(bannerType1());
 
      if(getType.contains("type-1")) {
-         block.append(bannerType1());
-     } else {
-        block.append(bannerType1());
+         block.append(bannerType1(block));
+     }else if(getType.contains("type-2")) {
+        block.append(bannerType2(block));
+     }else {
+        block.append(bannerType1(block));
      }
 }
 
 
-function bannerType1() {
+function bannerType1(block) {
+    block.closest(".promotionalbanner-container").classList.add("banner-varient1");
+    let source = block.querySelector("img").src;
+//     if(window.innerWidth > 767) {
+//     block.closest(".promotionalbanner-container").style.background =
+//   `url("${block.querySelector("img").src}") 0% 100% no-repeat #C8E5F5`;
+//     } else {
+// block.closest(".promotionalbanner-container").style.background =
+//   `url("${block.querySelector("img").src}") 0% 100% / contain no-repeat #C8E5F5`;
+//     }
+
     const promotionalBanner =
   div(
     {
@@ -23,7 +34,6 @@ function bannerType1() {
       "data-block-name": "promotionalbanner",
       "data-block-status": "loaded",
     },
-
     // -------- Image Section --------
     div({},
       div({},
@@ -32,10 +42,88 @@ function bannerType1() {
             fetchpriority: "high",
             alt: "",
             src:
-              "./media_1622bbba7242f74f6882b07c26ebb94a6b552fcd6.png?width=750&format=png&optimize=medium",
+              `${source}`,
             width: "750",
             height: "525",
           })
+      )
+    ),
+    // -------- Content Section --------
+    div({ class: "banner-conetent"},
+      div({},
+        h2(
+          { id: "upgrade-to-smarter-stronger-rewards" },
+          "Upgrade to smarter, stronger rewards."
+        ),
+        p(
+          {},
+          "Earn accelerated points, premium benefits, and access exclusive partner offers, all from one powerful card.fjwbfjsjfbsj"
+        ),
+        p({ class:"redirections"},
+          a(
+            { href: "/footer", title: "Explore Benefits" },
+            "Explore Benefits"
+          ),
+          " ",
+          a(
+            { href: "/us", title: "Know More" },
+            "Know More"
+          )
+        )
+      )
+    ),
+
+    // -------- Empty Config / Spacer --------
+    div({},
+      div({})
+    )
+  );
+          block.textContent = '';
+
+return promotionalBanner;
+}
+
+function bannerType2(block) {
+    block.closest(".promotionalbanner-container").classList.add("banner-varient2");
+    const promotionalBanner =
+  div(
+    {
+      class: "promotionalbanner block",
+      "data-block-name": "promotionalbanner",
+      "data-block-status": "loaded",
+    },
+
+    // -------- Image Section --------
+    div({},
+      div({},
+        picture({},
+          source({
+            type: "image/webp",
+            srcset:
+              "./media_1c9306d7674c0b8ee54457c9ac52f817a0d3410ae.png?width=2000&format=webply&optimize=medium",
+            media: "(min-width: 600px)",
+          }),
+          source({
+            type: "image/webp",
+            srcset:
+              "./media_1c9306d7674c0b8ee54457c9ac52f817a0d3410ae.png?width=750&format=webply&optimize=medium",
+          }),
+          source({
+            type: "image/png",
+            srcset:
+              "./media_1c9306d7674c0b8ee54457c9ac52f817a0d3410ae.png?width=2000&format=png&optimize=medium",
+            media: "(min-width: 600px)",
+          }),
+          img({
+            loading: "eager",
+            fetchpriority: "high",
+            alt: "",
+            src:
+              "./media_1c9306d7674c0b8ee54457c9ac52f817a0d3410ae.png?width=750&format=png&optimize=medium",
+            width: "750",
+            height: "525",
+          })
+        )
       )
     ),
 
@@ -48,7 +136,7 @@ function bannerType1() {
         ),
         p(
           {},
-          "Earn accelerated points, premium benefits, and access exclusive partner offers, all from one powerful card.fjwbfjsjfbsj"
+          "Earn accelerated points, premium benefits, and access exclusive partner offers, all from one powerful card."
         ),
         p({},
           a(
@@ -69,5 +157,8 @@ function bannerType1() {
       div({})
     )
   );
+
+          block.textContent = '';
+
 return promotionalBanner;
 }
