61fd0abbc5f74966f7343408e9d01d475e635480
Kishan Waghela <kishan.waghela@deptagency.com>
2026-01-19 08:49:05 +0530

    Faq Add

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -17,10 +17,50 @@ export default function decorate(block) {
     const body = row.children[1];
     body.className = "accordion-item-body";
     // decorate accordion item
+    // const uls = body.querySelectorAll("ul");
+    // uls.forEach((ul) => {
+    //   const ulWrapper = document.createElement("div");
+    //   ulWrapper.className = "accordion-ul-wrapper";
+    //   ul.parentNode.insertBefore(ulWrapper, ul);
+    //   ulWrapper.appendChild(ul);
+    // });
+
+    const uls = body.querySelectorAll("ul");
+    if (uls.length) {
+      const ulWrapper = document.createElement("div");
+      ulWrapper.className = "accordion-ul-wrapper";
+
+      uls[0].parentNode.insertBefore(ulWrapper, uls[0]);
+      uls.forEach((ul) => ulWrapper.appendChild(ul));
+    }
     const details = document.createElement("details");
     moveInstrumentation(row, details);
     details.className = "accordion-item";
     details.append(summary, body);
     row.replaceWith(details);
   });
+  // ---- container-level logic (run once) ----
+  const container = block.closest(".accordion-container");
+  if (!container) return;
+
+  if (container.querySelector(".accordion-item-wrapper")) return;
+  const accordions = container.querySelectorAll(":scope > .accordion-wrapper");
+  if (!accordions.length) return;
+
+  const accordionWrapper = document.createElement("div");
+  accordionWrapper.className = "accordion-item-wrapper";
+
+  accordions.forEach((acc) => accordionWrapper.appendChild(acc));
+  container.appendChild(accordionWrapper);
+
+  const wrapperItems = container.querySelectorAll(
+    ":scope > div:nth-of-type(2), :scope > div:nth-of-type(3)"
+  );
+
+  if (wrapperItems.length) {
+    const combinedWrapper = document.createElement("div");
+    combinedWrapper.className = "combined-wrapper";
+    container.insertBefore(combinedWrapper, wrapperItems[0]);
+    wrapperItems.forEach((item) => combinedWrapper.appendChild(item));
+  }
 }
