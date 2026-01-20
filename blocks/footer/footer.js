7e0bcbc405e955650ee1c6e66c346e12b8647f6f
Arbaz-Khan19e <Arbaz.Ahmed@teknopoint.local>
2026-01-20 18:03:05 +0530

    added footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -41,6 +41,5 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
-  block.append(footer);
+console.log("ffoter");
 }
