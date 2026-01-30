22e188bcab7f96a52bbad690fb6590b92fec7b72
Arbaz-Khan19e <Arbaz.Ahmed@teknopoint.local>
2026-01-30 12:59:44 +0530

    added hitech-gallery component

--- blocks/hitech-gallery/hitech-gallery.js
+++ blocks/hitech-gallery/hitech-gallery.js
@@ -7,23 +7,6 @@ import { readBlockConfig } from '../../scripts/aem.js';
  * @param {Element} block The theme configurator block element
  */
 export default async function decorate(block) {
-  // Read configuration from first row
-  const config = readBlockConfig(block);
-  const blockId = config.id || '';
-
-  // Set the ID on the block if provided
-  if (blockId) {
-    block.id = blockId;
-  }
-
-  // Add a wrapper class for styling
-  const variablesContainer = document.createElement('div');
-  variablesContainer.className = 'theme-configurator-variables';
-
-  // Move all remaining content (css-variable blocks) into the container
-  while (block.firstChild) {
-    variablesContainer.appendChild(block.firstChild);
-  }
-
-  block.appendChild(variablesContainer);
+console.log(block);
+  // block.appendChild(variablesContainer);
 }
